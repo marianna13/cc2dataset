b99eb0ef2ef56a0bbb41aa11d74707c90f36804e
Marianna Nezhurina <marianna@cpu128-dy-c6i-32xlarge-2.hpc-1click-ingress-east.pcluster>
2023-11-12 11:48:24 +0000

    add yt-dlp platforms extractor

--- cc2dataset/main.py
+++ cc2dataset/main.py
@@ -18,6 +18,55 @@ import time
 from .spark_session_builder import build_spark_session
 from io import BytesIO
 from urllib.parse import urljoin
+from yt_dlp.extractor import gen_extractors
+from typing import List
+from tqdm import tqdm
+
+def check_ext(ext, media_ext=List[str]) -> bool:
+    """Check a yt-dlp extractor"""
+
+    if 'porn' in ext.IE_NAME.lower() or ext.IE_NAME == 'generic':
+        return False
+    try:
+        tests = ext._TESTS
+        for test in tests:
+            if test['info_dict']['ext'] in media_ext:
+                if test['info_dict'].get('age_limit') is None:
+                    return True
+        return False
+    except:
+        return False
+
+platform_extension = {
+        "video": ["mp4", "m4a", "mov"],
+        "audio": ["mp3", "wav", "flac", "ogg", "opus"]
+    }
+
+class PlatformExtractor:
+    
+
+    def __init__(self, platform_type: str, platforms:list=None):
+        self.platform_type = platform_type
+        extractors = gen_extractors()
+        
+        if platforms:
+            extractors = [ext for ext in extractors if ext.IE_NAME in platforms]
+        else:
+            media_ext = platform_extension[platform_type.split("_")[0]]
+            extractors = [ext for ext in extractors if check_ext(ext, media_ext)]
+        self.extractors = extractors
+    
+    def check_url(self, url: str) -> bool:
+        """Check an arbitrary URL whether it's a platform link"""
+        if url is None:
+            return False
+        if not url.startswith("http://") or url.startswith("https://"):
+            return False
+        for ext in self.extractors:
+            if ext._match_valid_url(url):
+                return True
+        return False
+
 
 
 def valid_video_link(link):
@@ -29,6 +78,16 @@ def valid_video_link(link):
 
 def extract_video_from_links(links):
     filtered_links = [{"url": link["url"], "alt": link.get("text", "")} for link in links if valid_video_link(link)]
+    return filtered_links
+
+def extract_video_platform_from_links(links, platforms):
+    platform_extractor = PlatformExtractor("video_platform")
+    filtered_links = [{"url": link["url"], "alt": link.get("text", "")} for link in links if platform_extractor.check_url(link.get('url'))]
+    return filtered_links
+
+def extract_audio_platform_from_links(links):
+    platform_extractor = PlatformExtractor("audio_platform")
+    filtered_links = [{"url": link["url"], "alt": link.get("text", "")} for link in links if platform_extractor.check_url(link.get('url'))]
     return filtered_links
 
 
@@ -111,10 +170,10 @@ def make_link_absolute(url, base_url):
 
 
 def make_links_absolute(links, base_url):
-    return [{"url": make_link_absolute(link["url"], base_url), "alt": link["alt"]} for link in links]
+    return [{"url": make_link_absolute(link.get("url", ""), base_url), "alt": link.get("alt")} for link in links]
 
 
-def extract_documents_from_links(links, document_type):
+def extract_documents_from_links(links, document_type, platforms):
     """Extract documents from links ; this function returns a list of dict {"alt": ..., "url": ...}"""
 
     if document_type == "image":
@@ -127,15 +186,17 @@ def extract_documents_from_links(links, document_type):
         return extract_text_from_links(links)
     elif document_type == "video":
         return extract_video_from_links(links)
+    elif document_type == "video_platform" or platforms:
+        return extract_video_platform_from_links(links, platforms)
     else:
         raise ValueError(f"Unknown document type {document_type}")
 
 
-def extract_documents_from_wat(stream, document_type):
+def extract_documents_from_wat(stream, document_type, platforms):
     """Extract document from stream"""
     all_links = []
     try:
-        for record in ArchiveIterator(stream, record_types=WarcRecordType.metadata, parse_http=False):
+        for record in tqdm(ArchiveIterator(stream, record_types=WarcRecordType.metadata, parse_http=False)):
             try:
                 record_data = simdjson.load(record.reader)  # type: ignore
             except:  # pylint: disable=bare-except
@@ -162,14 +223,15 @@ def extract_documents_from_wat(stream, document_type):
                     base_url = urljoin(base_url, metadata["Head"]["Base"])
                 except ValueError:
                     pass
-
-            filtered_links = extract_documents_from_links(links, document_type)
+            
+            
+            filtered_links = extract_documents_from_links(links, document_type, platforms)
             filtered_links = make_links_absolute(filtered_links, base_url)
-            filtered_links = [
-                link
-                for link in filtered_links
-                if link["url"].startswith("http://") or link["url"].startswith("https://")
-            ]
+            # filtered_links = [
+            #     link
+            #     for link in filtered_links
+            #     if link["url"].startswith("http://") or link["url"].startswith("https://")
+            # ]
             for link in filtered_links:
                 link["uid"] = str(hashlib.md5((link["alt"] + link["url"]).encode()).hexdigest())
                 link["cc_filename"] = cc_filename
@@ -183,7 +245,7 @@ def extract_documents_from_wat(stream, document_type):
     return all_links
 
 
-def process_wat(path, document_type):
+def process_wat(path, document_type, platforms):
     """Process a single wat file"""
     begin_read = timer()
     with fsspec.open(path, "rb") as f:
@@ -199,7 +261,7 @@ def process_wat(path, document_type):
                 logger.info(f"retrying reading {i}/10")
                 time.sleep(1)
 
-        for e in extract_documents_from_wat(tf, document_type):
+        for e in extract_documents_from_wat(tf, document_type, platforms):
             yield (e["uid"], e["url"], e["alt"], e["cc_filename"], e["page_url"])
     end_read = timer()
     tot_read_time = end_read - begin_read
@@ -229,18 +291,25 @@ def get_cc_wat_links(source_cc_protocol):
 
 
 def read_wat_index_file(wat_index):
-    with fsspec.open(wat_index, "rb", compression="gzip") as f:
-        wats = [a.decode("utf8").strip() for a in f.readlines()]
+    while True:
+        try:
+            with fsspec.open(wat_index, "r", compression="gzip") as f:
+                index_lines = f.readlines()
+                break
+        except:
+            time.sleep(1)
+
+    wats = [a.strip() for a in index_lines]
     return wats
 
 
-def read_wat_index_files(shard_count, wat_count, source_cc_protocol):
+def read_wat_index_files(shard_count, wat_count, source_cc_protocol, num_threads):
     """Read all wat index files"""
     cc_wat_links = get_cc_wat_links(source_cc_protocol)
     if shard_count is not None:
         cc_wat_links = cc_wat_links[-shard_count:]  # pylint: disable=invalid-unary-operand-type
     all_wats = []
-    with ThreadPool(16) as pool:
+    with ThreadPool(num_threads) as pool:
         for wats in pool.imap_unordered(read_wat_index_file, cc_wat_links):
             all_wats.extend(wats)
     if wat_count is not None:
@@ -257,7 +326,7 @@ def deduplicate_repartition_count(df, output_path, wat_count, spark, shuffle=Fal
     s = time.time()
     if shuffle:
         uniques = uniques.sort(rand())
-    repartitioned = uniques.repartition(max(256, wat_count // 500))
+    repartitioned = uniques.repartition(max(1, wat_count // 500))
     repartitioned.write.mode("overwrite").parquet(output_path)
     e = time.time()
     logger.info(f"Took {e - s} seconds")
@@ -266,7 +335,7 @@ def deduplicate_repartition_count(df, output_path, wat_count, spark, shuffle=Fal
     logger.info(f"Size: {df.count()}")
 
 
-def process_one_part(output_path, wat_index_files, build_spark, shuffle, document_type, source_cc_protocol):
+def process_one_part(output_path, wat_index_files, build_spark, shuffle, document_type, source_cc_protocol, platforms):
     """Process one part"""
     spark = build_spark()
     sc = SparkContext.getOrCreate()
@@ -279,7 +348,7 @@ def process_one_part(output_path, wat_index_files, build_spark, shuffle, documen
 
     def extract(x):
         x = list(x)
-        yield from process_wat(prefix + x[0], document_type)
+        yield from process_wat(prefix + x[0], document_type, platforms)
 
     output = wat_rdd.mapPartitions(extract)
     df = output.toDF(["uid", "url", "alt", "cc_filename", "page_url"])
@@ -297,7 +366,7 @@ def get_last_successful_part(output_path):
 
 
 def process_multi_part(
-    output_path, wat_index_files, build_spark, multipart, shuffle, resume, document_type, source_cc_protocol
+    output_path, wat_index_files, build_spark, multipart, shuffle, resume, document_type, source_cc_protocol, platforms
 ):
     """Process multi part"""
     if resume:
@@ -314,7 +383,7 @@ def process_multi_part(
         part_path = f"{output_path}/part_{i}"
         part_paths.append(part_path)
         logger.info(f"Processing part {i} from {start} to {end} into {part_path}")
-        process_one_part(part_path, wat_index_files[start:end], build_spark, False, document_type, source_cc_protocol)
+        process_one_part(part_path, wat_index_files[start:end], build_spark, False, document_type, source_cc_protocol, platforms)
 
     spark = build_spark()
     logger.info("Merging parts")
@@ -346,6 +415,8 @@ def cc2dataset(
     spark_builder=None,
     document_type="image",
     source_cc_protocol="s3",
+    num_threads=16,
+    platforms=None
 ):
     """Convert common crawl to image caption set"""
 
@@ -371,7 +442,7 @@ def cc2dataset(
         return spark_builder()
 
     if resume is None:
-        wat_index_files = read_wat_index_files(wat_index_count, wat_count, source_cc_protocol)
+        wat_index_files = read_wat_index_files(wat_index_count, wat_count, source_cc_protocol, num_threads)
         # write wat index files to disk in output_path with fsspec
         with fsspec.open(f"{output_path}/wat_index_files.txt", "w", encoding="utf8") as f:
             f.write("\n".join(wat_index_files))
@@ -380,10 +451,10 @@ def cc2dataset(
             wat_index_files = f.read().splitlines()
 
     if multipart is None:
-        process_one_part(output_path, wat_index_files, build_spark, shuffle, document_type, source_cc_protocol)
+        process_one_part(output_path, wat_index_files, build_spark, shuffle, document_type, source_cc_protocol, platforms)
     else:
         process_multi_part(
-            output_path, wat_index_files, build_spark, multipart, shuffle, resume, document_type, source_cc_protocol
+            output_path, wat_index_files, build_spark, multipart, shuffle, resume, document_type, source_cc_protocol, platforms
         )
 
 
